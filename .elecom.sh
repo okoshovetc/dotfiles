98ee20ab60aa293b3f9f997ffca34929e4768cb4
Oleg Koshovets <okoshovetc@gmail.com>
2021-01-15 03:18:22 +0300

    add trackball script

---
+++ .elecom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop "ELECOM TrackBall Mouse HUGE TrackBall" "Evdev Wheel Emulation" 1
+xinput --set-prop "ELECOM TrackBall Mouse HUGE TrackBall" "Evdev Wheel Emulation Button" 12
